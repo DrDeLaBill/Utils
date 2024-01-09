3e208087b668ba92cee134b091a91c3fe593d4dc
DrDeLaBill <40359652+DrDeLaBill@users.noreply.github.com>
2024-01-09 11:13:56 +0400

    Archive fix

--- utils/Archive/archiver.c
+++ utils/Archive/archiver.c
@@ -8,16 +8,16 @@
 
 void zip(uint8_t* src, uint8_t* arch, uint32_t src_len, uint32_t arch_len) 
 {
-    (void*)src;
-    (void*)arch;
+    (void)src;
+    (void)arch;
     (void)src_len;
     (void)arch_len;
 }
 
 void unzip(uint8_t* arch, uint8_t* dst, uint32_t arch_len, uint32_t dst_len)
 {
-    (void*)dst;
-    (void*)arch;
+    (void)dst;
+    (void)arch;
     (void)dst_len;
     (void)arch_len;
 }
