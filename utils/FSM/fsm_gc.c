c78dd790dcd0000784208e1a383b260eca15ea18
DrDeLaBill <40359652+DrDeLaBill@users.noreply.github.com>
2024-06-14 18:58:22 +0400

    GC FSM update

--- utils/FSM/fsm_gc.c
+++ utils/FSM/fsm_gc.c
@@ -2,8 +2,13 @@
 
 #include "fsm_gc.h"
 
+#include <string.h>
+
 #include "utils.h"
 #include "bmacro.h"
+
+
+static size_t _fsm_gc_events_count = 1;
 
 
 void fsm_gc_init(fsm_gc_t* fsm, fsm_gc_transition_t* table, unsigned size)
@@ -25,21 +30,14 @@ void fsm_gc_init(fsm_gc_t* fsm, fsm_gc_transition_t* table, unsigned size)
     }
 }
 
-void fsm_gc_push_event(fsm_gc_t* fsm, fsm_gc_event_t* event)
-{
-    if (fsm->_events_count >= __arr_len(fsm->_events) - 1) {
-        for (unsigned i = 0; i < __arr_len(fsm->_events) - 1; i++) {
-            fsm->_events[i] = fsm->_events[i + 1];
-        }
-        fsm->_events_count = __arr_len(fsm->_events) - 1;
-    }
-    fsm->_events[fsm->_events_count++] = *event;
-}
-
 void fsm_gc_proccess(fsm_gc_t* fsm) 
 {
+    if (!fsm) {
+        BEDUG_ASSERT(false, "Empty FSM pointer");
+        return;
+    }
     BEDUG_ASSERT(fsm->_initialized, "FSM has not initialized");
-    if (fsm->_state && fsm->_table) {
+    if (fsm->_initialized && fsm->_state && fsm->_table) {
         fsm->_state->state();
         for (unsigned i = 0; i < fsm->_events_count; i++) {
             fsm_gc_event_t event = fsm->_events[i];
@@ -60,4 +58,34 @@ void fsm_gc_proccess(fsm_gc_t* fsm)
     } else {
         BEDUG_ASSERT(false, "FSM state and event must not be NULL");
     }
+}
+
+void fsm_gc_push_event(fsm_gc_t* fsm, fsm_gc_event_t* event)
+{
+    if (!fsm) {
+        BEDUG_ASSERT(false, "Empty FSM pointer");
+        return;
+    }
+    if (!event) {
+        BEDUG_ASSERT(false, "Empty FSM event pointer");
+        return;
+    }
+    BEDUG_ASSERT(fsm->_initialized, "FSM has not initialized");
+    if (fsm->_events_count >= __arr_len(fsm->_events) - 1) {
+        for (unsigned i = 0; i < __arr_len(fsm->_events) - 1; i++) {
+            fsm->_events[i] = fsm->_events[i + 1];
+        }
+        fsm->_events_count = __arr_len(fsm->_events) - 1;
+    }
+    fsm->_events[fsm->_events_count++] = *event;
+}
+
+void fsm_gc_clear(fsm_gc_t* fsm)
+{
+    if (!fsm) {
+        BEDUG_ASSERT(false, "Empty FSM pointer");
+        return;
+    }
+    BEDUG_ASSERT(fsm->_initialized, "FSM has not initialized");
+	memset(fsm->_events, 0, sizeof(fsm->_events));
 }
