0025aa836b59f886420f6b16557362431675d79d
DrDeLaBill <40359652+DrDeLaBill@users.noreply.github.com>
2024-08-02 13:13:06 +0400

    debug fix

--- utils/Debug/bedug.c
+++ utils/Debug/bedug.c
@@ -11,7 +11,7 @@ void b_assert(const char* file, int line, const char* message)
     gprint("%s (%d) : bedug assert failed, THERE IS NO REASON TO PANIC, the system is stable : %s\n", file, line, message);
 }
 #else
-void b_assert(const char*, int, const char*)
+void b_assert(const char* file, int line, const char* message)
 {
 	// TODO: write type of error to the error list and save in memory
 }
