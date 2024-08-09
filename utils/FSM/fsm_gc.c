dab9009f4cda4deb1c10396af63c871e2b818970
DrDeLaBill <40359652+DrDeLaBill@users.noreply.github.com>
2024-08-09 12:21:50 +0400

    FSM event prioprity and actions added

--- utils/FSM/fsm_gc.c
+++ utils/FSM/fsm_gc.c
@@ -17,7 +17,8 @@ void fsm_gc_init(fsm_gc_t* fsm, fsm_gc_transition_t* table, unsigned size)
         return;
     }
     if (!size || !table) {
-        BEDUG_ASSERT(false, "Empty FSM transition table");
+        BEDUG_ASSERT(!fsm->_e_fsm_tt, "Empty FSM transition table");
+        fsm->_e_fsm_tt = true;
         return;
     }
     fsm->_initialized = true;
@@ -35,27 +36,51 @@ void fsm_gc_proccess(fsm_gc_t* fsm)
         BEDUG_ASSERT(false, "Empty FSM pointer");
         return;
     }
-    BEDUG_ASSERT(fsm->_initialized, "FSM has not initialized");
+    if (!fsm->_initialized) {
+    	BEDUG_ASSERT(!fsm->_fsm_not_i, "FSM has not initialized");
+        BEDUG_ASSERT(!fsm->_fsm_not_i, "FSM state and event must not be NULL");
+        fsm->_fsm_not_i = true;
+    	return;
+    }
+
+    bool is_transition = false;
+	uint32_t table_idx = 0;
+	size_t event_idx   = 0;
+	size_t event_prio  = 0;
     if (fsm->_initialized && fsm->_state && fsm->_table) {
         fsm->_state->state();
         for (unsigned i = 0; i < fsm->_events_count; i++) {
             fsm_gc_event_t event = fsm->_events[i];
             for (unsigned j = 0; j < fsm->_table_size; j++) {
                 fsm_gc_transition_t tr = fsm->_table[j]; \
-                if (fsm->_state->state == tr.source->state && event.index == tr.event->index) {
-                    fsm->_state = tr.target;
-                    fsm->_events_count--;
-                    for (unsigned k = 0; k < fsm->_events_count; k++) {
-                        fsm->_events[k] = fsm->_events[k + 1];
-                    }
-                    i = fsm->_events_count;
-                    j = fsm->_table_size;
-                    break;
+                if (!(fsm->_state->state == tr.source->state && event.index == tr.event->index)) {
+                	continue;
                 }
+				if (!is_transition ||
+					event_prio < event.priority
+				) {
+					event_idx  = i;
+					table_idx  = j;
+					event_prio = event.priority;
+				}
+				is_transition = true;
             }
         } 
-    } else {
-        BEDUG_ASSERT(false, "FSM state and event must not be NULL");
+
+        if (!is_transition) {
+        	return;
+        }
+
+		fsm->_state = fsm->_table[table_idx].target;
+		fsm->_events_count--;
+		memset((void*)&fsm->_events[event_idx], 0, sizeof(fsm->_events[event_idx]));
+
+		for (unsigned i = event_idx; i < fsm->_events_count; i++) {
+			fsm->_events[i] = fsm->_events[i + 1];
+		}
+		if (fsm->_table[table_idx].action) {
+			fsm->_table[table_idx].action();
+		}
     }
 }
 
@@ -69,7 +94,11 @@ void fsm_gc_push_event(fsm_gc_t* fsm, fsm_gc_event_t* event)
         BEDUG_ASSERT(false, "Empty FSM event pointer");
         return;
     }
-    BEDUG_ASSERT(fsm->_initialized, "FSM has not initialized");
+    if (!fsm->_initialized) {
+    	BEDUG_ASSERT(!fsm->_fsm_not_i, "FSM has not initialized");
+        fsm->_fsm_not_i = true;
+    	return;
+    }
     if (fsm->_events_count >= __arr_len(fsm->_events) - 1) {
         for (unsigned i = 0; i < __arr_len(fsm->_events) - 1; i++) {
             fsm->_events[i] = fsm->_events[i + 1];
@@ -85,12 +114,29 @@ void fsm_gc_clear(fsm_gc_t* fsm)
         BEDUG_ASSERT(false, "Empty FSM pointer");
         return;
     }
-    BEDUG_ASSERT(fsm->_initialized, "FSM has not initialized");
+    if (!fsm->_initialized) {
+    	BEDUG_ASSERT(!fsm->_fsm_not_i, "FSM has not initialized");
+        fsm->_fsm_not_i = true;
+    	return;
+    }
 	memset(fsm->_events, 0, sizeof(fsm->_events));
 	fsm->_events_count = 0;
 }
 
 bool fsm_gc_is_state(fsm_gc_t* fsm, fsm_gc_state_t* state)
 {
+    if (!fsm) {
+        BEDUG_ASSERT(false, "Empty FSM pointer");
+        return false;
+    }
+    if (!state) {
+        BEDUG_ASSERT(false, "Empty FSM state pointer");
+        return false;
+    }
+    if (!fsm->_initialized) {
+    	BEDUG_ASSERT(!fsm->_fsm_not_i, "FSM has not initialized");
+        fsm->_fsm_not_i = true;
+    	return false;
+    }
 	return fsm->_state->state == state->state;
 }
--- utils/FSM/fsm_gc.h
+++ utils/FSM/fsm_gc.h
@@ -20,6 +20,7 @@ extern "C" {
 
 typedef struct _fsm_gc_event_t {
     size_t index;
+    size_t priority;
 } fsm_gc_event_t;
 
 typedef struct _fsm_gc_state_t {
@@ -30,6 +31,7 @@ typedef struct _fsm_gc_transition_t {
     fsm_gc_state_t* source;
     fsm_gc_event_t* event;
     fsm_gc_state_t* target;
+    void (*action) (void);
 } fsm_gc_transition_t;
 
 typedef struct _fsm_gc_t {
@@ -39,16 +41,32 @@ typedef struct _fsm_gc_t {
     fsm_gc_event_t       _events[_FSM_GC_EVENTS_COUNT];
     fsm_gc_transition_t* _table;
     size_t               _table_size;
+#ifdef DEBUG
+    bool                 _e_fsm_tt;
+    bool                 _fsm_not_i;
+#endif
 } fsm_gc_t;
 
 
 #define FSM_GC_CREATE_STATE(NAME, FUNC)       fsm_gc_state_t NAME = { FUNC };
 
-#define FSM_GC_CREATE_EVENT(NAME)             fsm_gc_event_t NAME = { 0 };
+#define FSM_GC_CREATE_EVENT(NAME, PRIO)       fsm_gc_event_t NAME = { 0, PRIO };
                                               
 #define FSM_GC_CREATE_TABLE(NAME, ...)        fsm_gc_transition_t NAME[] = { __VA_ARGS__ };
 
-#define FSM_GC_CREATE(FSM_NAME)               fsm_gc_t FSM_NAME = { \
+#ifdef DEBUG
+#   define FSM_GC_CREATE(FSM_NAME)            fsm_gc_t FSM_NAME = { \
+                                                  /* ._initialized = */  false, \
+                                                  /* ._state = */        NULL, \
+                                                  /* ._events_count = */ 0, \
+                                                  /* ._events = */       {0}, \
+                                                  /* ._table = */        NULL, \
+                                                  /* ._table_size = */   0, \
+	                                              /* _e_fsm_tt = */      false, \
+	                                              /* _fsm_not_i = */     false \
+                                              };
+#else
+#   define FSM_GC_CREATE(FSM_NAME)            fsm_gc_t FSM_NAME = { \
                                                   /* ._initialized = */  false, \
                                                   /* ._state = */        NULL, \
                                                   /* ._events_count = */ 0, \
@@ -56,6 +74,7 @@ typedef struct _fsm_gc_t {
                                                   /* ._table = */        NULL, \
                                                   /* ._table_size = */   0 \
                                               };
+#endif
 
 
 void fsm_gc_init(fsm_gc_t* fsm, fsm_gc_transition_t* table, unsigned size);
