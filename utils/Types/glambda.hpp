c74b8c366e7bfa65cbf890659e616f8177e8a5f0
DrDeLaBill <georgmix301@gmail.com>
2026-02-08 22:33:12 +0400

    glambda update

--- utils/Types/glambda.hpp
+++ utils/Types/glambda.hpp
@@ -7,14 +7,17 @@
 #include <functional>
 
 
-#define G_LAMBDA_ITERATOR_CREATE(...) static std::initializer_list<std::function<void()>> _g_lambda_functors = { __VA_ARGS__ };
+#define G_LAMBDA_ITERATOR_CREATE(...) static std::initializer_list<std::function<void()>> _g_lambda_functors = { __VA_ARGS__ }; static size_t _g_lambda_iterator = 0;
 
 #define G_LAMBDA_ITERATOR_TICK() { \
-            static size_t _g_lambda_iterator = 0; \
             if (_g_lambda_iterator >= _g_lambda_functors.size()) { \
                 _g_lambda_iterator = 0; \
             } \
             (*(_g_lambda_functors.begin() + _g_lambda_iterator++))(); \
+        }
+
+#define G_LAMBDA_ITERATOR_RESET() { \
+            _g_lambda_iterator = 0; \
         }
 
 
