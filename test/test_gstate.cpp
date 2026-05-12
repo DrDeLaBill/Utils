c4124604b707901684edc3d45e423b46811e4549
DrDeLaBill <georgmix301@gmail.com>
2026-05-12 18:13:26 +0400

    GState tests update

--- test/test_gstate.cpp
+++ test/test_gstate.cpp
@@ -22,6 +22,7 @@ public:
 GSTATE_CREATE_STATE(GSV_ValidInit_state1_s, _GSV_ValidInit_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_ValidInit_states, &GSV_ValidInit_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_ValidInit_gstate)
+void _GSV_ValidInit_state1_s() {}
 TEST(GSTATE_Fixture, ValidInit)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_ValidInit_gstate, GSTATE_SUITE_ValidInit_states, __arr_len(GSTATE_SUITE_ValidInit_states)));
@@ -41,6 +42,8 @@ GSTATE_CREATE_STATE(GSV_StateTransition_state1_s, _GSV_StateTransition_state1_s,
 GSTATE_CREATE_STATE(GSV_StateTransition_state2_s, _GSV_StateTransition_state2_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_StateTransition_states, &GSV_StateTransition_state1_s, &GSV_StateTransition_state2_s);
 GSTATE_CREATE(GSTATE_SUITE_StateTransition_gstate)
+void _GSV_StateTransition_state1_s() {}
+void _GSV_StateTransition_state2_s() {}
 TEST(GSTATE_Fixture, StateTransition)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_StateTransition_gstate, GSTATE_SUITE_StateTransition_states, __arr_len(GSTATE_SUITE_StateTransition_states)));
@@ -53,6 +56,7 @@ TEST(GSTATE_Fixture, StateTransition)
 GSTATE_CREATE_STATE(GSV_ClearQueue_state1_s, _GSV_ClearQueue_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_ClearQueue_states, &GSV_ClearQueue_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_ClearQueue_gstate)
+void _GSV_ClearQueue_state1_s() {}
 TEST(GSTATE_Fixture, ClearQueue)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_ClearQueue_gstate, GSTATE_SUITE_ClearQueue_states, __arr_len(GSTATE_SUITE_ClearQueue_states)));
@@ -66,6 +70,8 @@ GSTATE_CREATE_STATE(GSV_IsStateCheck_state1_s, _GSV_IsStateCheck_state1_s, 0);
 GSTATE_CREATE_STATE(GSV_IsStateCheck_state2_s, _GSV_IsStateCheck_state2_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_IsStateCheck_states, &GSV_IsStateCheck_state1_s, &GSV_IsStateCheck_state2_s);
 GSTATE_CREATE(GSTATE_SUITE_IsStateCheck_gstate)
+void _GSV_IsStateCheck_state1_s() {}
+void _GSV_IsStateCheck_state2_s() {}
 TEST(GSTATE_Fixture, IsStateCheck)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_IsStateCheck_gstate, GSTATE_SUITE_IsStateCheck_states, __arr_len(GSTATE_SUITE_IsStateCheck_states)));
@@ -79,6 +85,9 @@ GSTATE_CREATE_STATE(GSV_Priority_state2_s, _GSV_Priority_state2_s, 2);
 GSTATE_CREATE_STATE(GSV_Priority_state3_s, _GSV_Priority_state3_s, 1);
 GSTATE_CREATE_LIST(GSTATE_SUITE_Priority_states, &GSV_Priority_state1_s, &GSV_Priority_state2_s, &GSV_Priority_state3_s);
 GSTATE_CREATE(GSTATE_SUITE_Priority_gstate)
+void _GSV_Priority_state1_s() {}
+void _GSV_Priority_state2_s() {}
+void _GSV_Priority_state3_s() {}
 TEST(GSTATE_Fixture, PrioritySelection)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_Priority_gstate, GSTATE_SUITE_Priority_states, __arr_len(GSTATE_SUITE_Priority_states)));
@@ -97,6 +106,8 @@ GSTATE_CREATE_STATE(GSV_NoTransition_state1_s, _GSV_NoTransition_state1_s, 0);
 GSTATE_CREATE_STATE(GSV_NoTransition_fake_s, _GSV_NoTransition_fake_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_NoTransition_states, &GSV_NoTransition_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_NoTransition_gstate)
+void _GSV_NoTransition_state1_s() {}
+void _GSV_NoTransition_fake_s() {}
 TEST(GSTATE_Fixture, NoTransition)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_NoTransition_gstate, GSTATE_SUITE_NoTransition_states, __arr_len(GSTATE_SUITE_NoTransition_states)));
@@ -110,6 +121,10 @@ static bool GSV_StateFunctionExecution_called = false;
 GSTATE_CREATE_STATE(GSV_StateFunctionExecution_state1_s, _GSV_StateFunctionExecution_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_StateFunctionExecution_states, &GSV_StateFunctionExecution_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_StateFunctionExecution_gstate)
+void _GSV_StateFunctionExecution_state1_s()
+{
+    GSV_StateFunctionExecution_called = true;
+}
 TEST(GSTATE_Fixture, StateFunctionExecution)
 {
     GSV_StateFunctionExecution_called = false;
@@ -123,6 +138,8 @@ GSTATE_CREATE_STATE(GSV_ProcessCycle_stateA_s, _GSV_ProcessCycle_stateA_s, 0);
 GSTATE_CREATE_STATE(GSV_ProcessCycle_stateB_s, _GSV_ProcessCycle_stateB_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_ProcessCycle_states, &GSV_ProcessCycle_stateA_s, &GSV_ProcessCycle_stateB_s);
 GSTATE_CREATE(GSTATE_SUITE_ProcessCycle_gstate)
+void _GSV_ProcessCycle_stateA_s() {}
+void _GSV_ProcessCycle_stateB_s() {}
 TEST(GSTATE_Fixture, ProcessCycle)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_ProcessCycle_gstate, GSTATE_SUITE_ProcessCycle_states, __arr_len(GSTATE_SUITE_ProcessCycle_states)));
@@ -138,6 +155,11 @@ GSTATE_CREATE_STATE(GSV_StateDriven_state1_s, _GSV_StateDriven_state1_s, 0);
 GSTATE_CREATE_STATE(GSV_StateDriven_state2_s, _GSV_StateDriven_state2_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_StateDriven_states, &GSV_StateDriven_state1_s, &GSV_StateDriven_state2_s);
 GSTATE_CREATE(GSTATE_SUITE_StateDriven_gstate)
+void _GSV_StateDriven_state1_s()
+{
+    gstate_request_state(&GSTATE_SUITE_StateDriven_gstate, &GSV_StateDriven_state2_s);
+}
+void _GSV_StateDriven_state2_s() {}
 TEST(GSTATE_Fixture, StateDrivenRequests)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_StateDriven_gstate, GSTATE_SUITE_StateDriven_states, __arr_len(GSTATE_SUITE_StateDriven_states)));
@@ -150,6 +172,10 @@ static int GSV_EmptyQueue_exec_count = 0;
 GSTATE_CREATE_STATE(GSV_EmptyQueue_state1_s, _GSV_EmptyQueue_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_EmptyQueue_states, &GSV_EmptyQueue_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_EmptyQueue_gstate)
+void _GSV_EmptyQueue_state1_s()
+{
+    GSV_EmptyQueue_exec_count++;
+}
 TEST(GSTATE_Fixture, EmptyQueueProcessing)
 {
     GSV_EmptyQueue_exec_count = 0;
@@ -165,6 +191,8 @@ GSTATE_CREATE_STATE(GSV_Termination_state1_s, _GSV_Termination_state1_s, 0);
 GSTATE_CREATE_STATE(GSV_Termination_state2_s, _GSV_Termination_state2_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_Termination_states, &GSV_Termination_state1_s, &GSV_Termination_state2_s);
 GSTATE_CREATE(GSTATE_SUITE_Termination_gstate)
+void _GSV_Termination_state1_s() {}
+void _GSV_Termination_state2_s() {}
 TEST(GSTATE_Fixture, TerminationCondition)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_Termination_gstate, GSTATE_SUITE_Termination_states, __arr_len(GSTATE_SUITE_Termination_states)));
@@ -184,17 +212,20 @@ TEST(GSTATE_Fixture, TerminationCondition)
 GSTATE_CREATE_STATE(GSV_CheckStatesCount_state1_s, _GSV_CheckStatesCount_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_CheckStatesCount_states, &GSV_CheckStatesCount_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_CheckStatesCount_gstate)
+void _GSV_CheckStatesCount_state1_s() {}
 TEST(GSTATE_Fixture, CheckStatesCount)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_CheckStatesCount_gstate, GSTATE_SUITE_CheckStatesCount_states, __arr_len(GSTATE_SUITE_CheckStatesCount_states)));
     ASSERT_EQ(GSTATE_SUITE_CheckStatesCount_gstate._states_count, 1u);
 }
 
-// Unique Test A: Deduplication
+// Test 14: Deduplication
 GSTATE_CREATE_STATE(GSV_Dedup_stateA_s, _GSV_Dedup_stateA_s, 0);
 GSTATE_CREATE_STATE(GSV_Dedup_stateB_s, _GSV_Dedup_stateB_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_Dedup_states, &GSV_Dedup_stateA_s, &GSV_Dedup_stateB_s, &GSV_Dedup_stateA_s);
 GSTATE_CREATE(GSTATE_SUITE_Dedup_gstate)
+void _GSV_Dedup_stateA_s() {}
+void _GSV_Dedup_stateB_s() {}
 TEST(GSTATE_Fixture, DeduplicationRemovesDuplicatesAndEmpty)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_Dedup_gstate, GSTATE_SUITE_Dedup_states, __arr_len(GSTATE_SUITE_Dedup_states)));
@@ -202,10 +233,11 @@ TEST(GSTATE_Fixture, DeduplicationRemovesDuplicatesAndEmpty)
     ASSERT_TRUE(gstate_is_state(&GSTATE_SUITE_Dedup_gstate, &GSV_Dedup_stateA_s));
 }
 
-// Unique Test B: RequestNullTargetIsIgnored
+// Test 15: RequestNullTargetIsIgnored
 GSTATE_CREATE_STATE(GSV_RequestNull_state1_s, _GSV_RequestNull_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_RequestNull_states, &GSV_RequestNull_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_RequestNull_gstate)
+void _GSV_RequestNull_state1_s() {}
 TEST(GSTATE_Fixture, RequestNullTargetIsIgnored)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_RequestNull_gstate, GSTATE_SUITE_RequestNull_states, __arr_len(GSTATE_SUITE_RequestNull_states)));
@@ -214,10 +246,11 @@ TEST(GSTATE_Fixture, RequestNullTargetIsIgnored)
     ASSERT_EQ(GSTATE_SUITE_RequestNull_gstate._queue_cnt, 0);
 }
 
-// Unique Test C: RequestingCurrentStateIsIgnored
+// Test 16: RequestingCurrentStateIsIgnored
 GSTATE_CREATE_STATE(GSV_RequestSame_state1_s, _GSV_RequestSame_state1_s, 0);
 GSTATE_CREATE_LIST(GSTATE_SUITE_RequestSame_states, &GSV_RequestSame_state1_s);
 GSTATE_CREATE(GSTATE_SUITE_RequestSame_gstate)
+void _GSV_RequestSame_state1_s() {}
 TEST(GSTATE_Fixture, RequestingCurrentStateIsIgnored)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_RequestSame_gstate, GSTATE_SUITE_RequestSame_states, __arr_len(GSTATE_SUITE_RequestSame_states)));
@@ -227,11 +260,13 @@ TEST(GSTATE_Fixture, RequestingCurrentStateIsIgnored)
     ASSERT_EQ(GSTATE_SUITE_RequestSame_gstate._queue_cnt, 0);
 }
 
-// Unique Test D: Reset behavior
+// Test 17: Reset behavior
 GSTATE_CREATE_STATE(GSV_Reset_state1_s, _GSV_Reset_state1_s, 0);
 GSTATE_CREATE_STATE(GSV_Reset_state2_s, _GSV_Reset_state2_s, 1);
 GSTATE_CREATE_LIST(GSTATE_SUITE_ResetBehavior_states, NULL, &GSV_Reset_state1_s, NULL, &GSV_Reset_state2_s, NULL);
 GSTATE_CREATE(GSTATE_SUITE_ResetBehavior_gstate)
+void _GSV_Reset_state1_s() {}
+void _GSV_Reset_state2_s() {}
 TEST(GSTATE_Fixture, ResetRestoresFirstStateAndClearsQueue)
 {
     ASSERT_TRUE(gstate_init(&GSTATE_SUITE_ResetBehavior_gstate, GSTATE_SUITE_ResetBehavior_states, __arr_len(GSTATE_SUITE_ResetBehavior_states)));
@@ -244,53 +279,28 @@ TEST(GSTATE_Fixture, ResetRestoresFirstStateAndClearsQueue)
     ASSERT_EQ(GSTATE_SUITE_ResetBehavior_gstate._queue_cnt, 0);
 }
 
-// Unique Test E: Enable/Disable messages (no changes)
+// Test 18: Enable/Disable messages (no changes)
 TEST(GSTATE_Fixture, EnableDisableMessagesSafeCalls)
 {
-    // Calls should be safe both for real instance and nullptr (no crash)
     gstate_enable_all_messages();
     gstate_disable_all_messages();
-    gstate_enable_messages(nullptr); // no-op in non-debug
+    gstate_enable_messages(nullptr);
     gstate_disable_messages(nullptr);
     ASSERT_TRUE(true);
 }
 
+// Test 19: Check the broken states array
 GSTATE_CREATE_STATE(CheckEmptyInitState_gstate1_s, _CheckEmptyInitState_gstate1_s, 0);
-GSTATE_CREATE_LIST(CheckEmptyInitState_gstate_list, NULL, &CheckEmptyInitState_gstate1_s);
+GSTATE_CREATE_LIST(CheckEmptyInitState_gstate_list, NULL, &CheckEmptyInitState_gstate1_s, NULL);
 GSTATE_CREATE(CheckEmptyInitState_gstate)
+void _CheckEmptyInitState_gstate1_s() {}
 TEST(GSTATE_Fixture, CheckEmptyInitState)
 {
     ASSERT_TRUE(gstate_init(&CheckEmptyInitState_gstate, CheckEmptyInitState_gstate_list, __arr_len(CheckEmptyInitState_gstate_list)));
 
     ASSERT_TRUE(gstate_is_state(&CheckEmptyInitState_gstate, &CheckEmptyInitState_gstate1_s));
-}
 
-// -- Implementations for CREATED state functions (minimal) --
-// Provide function bodies matching the FUNC names passed to GSTATE_CREATE_STATE
-static void _GSV_ValidInit_state1_s(void) {}
-static void _GSV_StateTransition_state1_s(void) {}
-static void _GSV_StateTransition_state2_s(void) {}
-static void _GSV_ClearQueue_state1_s(void) {}
-static void _GSV_IsStateCheck_state1_s(void) {}
-static void _GSV_IsStateCheck_state2_s(void) {}
-static void _GSV_Priority_state1_s(void) {}
-static void _GSV_Priority_state2_s(void) {}
-static void _GSV_Priority_state3_s(void) {}
-static void _GSV_NoTransition_state1_s(void) {}
-static void _GSV_NoTransition_fake_s(void) {}
-static void _GSV_StateFunctionExecution_state1_s(void) { GSV_StateFunctionExecution_called = true; }
-static void _GSV_ProcessCycle_stateA_s(void) {}
-static void _GSV_ProcessCycle_stateB_s(void) {}
-static void _GSV_StateDriven_state1_s(void) { gstate_request_state(&GSTATE_SUITE_StateDriven_gstate, &GSV_StateDriven_state2_s); }
-static void _GSV_StateDriven_state2_s(void) {}
-static void _GSV_EmptyQueue_state1_s(void) { GSV_EmptyQueue_exec_count++; }
-static void _GSV_Termination_state1_s(void) {}
-static void _GSV_Termination_state2_s(void) {}
-static void _GSV_CheckStatesCount_state1_s(void) {}
-static void _GSV_Dedup_stateA_s(void) {}
-static void _GSV_Dedup_stateB_s(void) {}
-static void _GSV_RequestNull_state1_s(void) {}
-static void _GSV_RequestSame_state1_s(void) {}
-static void _GSV_Reset_state1_s(void) {}
-static void _GSV_Reset_state2_s(void) {}
-static void _CheckEmptyInitState_gstate1_s() {};
+    ASSERT_EQ(CheckEmptyInitState_gstate._states_count, 1);
+    ASSERT_EQ(CheckEmptyInitState_gstate._curr_state, &CheckEmptyInitState_gstate1_s);
+    ASSERT_EQ(CheckEmptyInitState_gstate._states_list[0], &CheckEmptyInitState_gstate1_s);
+}
